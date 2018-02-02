2cca9ad6200c8aaa4bbdfa8b4dcea5f0ac756250
Glitch (fcc-url-shortener-zdravkovski) <none>
2018-02-02 09:00:53 +0000

    📺📙 Added redirect functionality

--- server.js
+++ server.js
@@ -26,35 +26,61 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGOLAB_URI); 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('We\'re open for business!')); 
+
 //Shortener route
-app.get('/api/new/:url(*)', (req, res, next) => {
-  
-  /** this project needs a db !! **/ 
-  mongoose.connect(process.env.MONGOLAB_URI); 
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', () => console.log('We\'re open for business!')); 
-  
+app.get('/api/:url(*)', (req, res) => {
+    
   const inputURL = req.params.url;
-  const outputURL = Math.floor(Math.random() * 1000).toString();
+  const outputURL = Math.floor(Math.random() * 10000).toString();
   
   const data = new Short({
     longURL: inputURL,
     shortURL: outputURL
   });
+  
+  data.save( err => {
+    if (err) {
+      res.send('Error saving to DB'); 
+    }
+  });
 
   
   if (validUrl.isUri(inputURL)) {
-    res.json({ originalURL: inputURL,
-               
-             });  
+    res.json(data);  
   } else {
     res.json({ error: 'Not a valid URL' }); 
   }
   
-
+  res.json(data);
+  
 });
 
+app.get('/:forwardURL', (req, res) => {
   
+  const forward = req.params.forwardURL;
+  
+  Short.findOne({ shortURL: forward }, (err, data) => {
+
+    const regex = new RegExp('^(http|https)://', 'i');
+    const originalURL = data.longURL;
+    
+    if (err) {
+      res.json({ error: 'Error reading database' }); 
+    }
+    if (regex.test(originalURL)) {
+      res.redirect(301, data.longURL);
+    } else {
+      res.redirect(301, `http://${data.longURL}`); 
+    }
+    
+  });
+  
+});
+
 // your first API endpoint... 
 app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
